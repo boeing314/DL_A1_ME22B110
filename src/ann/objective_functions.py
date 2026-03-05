1c9ab5150e4f16256f62a8c6e54c3bbf79480003
Asher <216724465+boeing314@users.noreply.github.com>
2026-03-05 22:45:37 +0000

    initial commit

--- src/ann/objective_functions.py
+++ src/ann/objective_functions.py
@@ -14,10 +14,14 @@ class MSE:
 
 class cross_entropy:
     def forward(self,y_true,y_pred):
+        # We implement softmax and get the probability corresponding to each digit
         exp_logits=np.exp(y_pred-np.max(y_pred,axis=1,keepdims=True))
         self.probs=exp_logits/np.sum(exp_logits,axis=1,keepdims=True)
+        # We implement a categorical cross entropy loss as the classes are mutually exclusive
         B=y_true.shape[0]
         return -np.sum(y_true*np.log(self.probs+1e-12))/B
     def backward(self,y_true,y_pred):
+        exp_logits=np.exp(y_pred-np.max(y_pred,axis=1,keepdims=True))
+        self.probs=exp_logits/np.sum(exp_logits,axis=1,keepdims=True)
         B=y_true.shape[0]
         return (self.probs-y_true)/B
