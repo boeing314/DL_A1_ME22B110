b7bffa9cffd83e5452bc30028502ff32276ac728
Asher <216724465+boeing314@users.noreply.github.com>
2026-03-05 13:14:12 +0000

    initial commit

--- src/ann/neural_network.py
+++ src/ann/neural_network.py
@@ -57,21 +57,23 @@ class NeuralNetwork:
         self.layers.append(neural_layer.NeuralLayer(self.hidden_size[-1], self.output_size, self.weight_init))
     
     def get_weights(self):
-        weights = {}
+        weights = []
 
-        for i, layer in enumerate(self.layers):
-            weights[f"W{i+1}"] = layer.W
-            weights[f"b{i+1}"] = layer.b
+        for layer in self.layers:
+            weights.append({
+                "W": layer.W,
+                "b": layer.b
+            })
 
         return weights
 
     def set_weights(self, weights):
-        for i, layer in enumerate(self.layers):
-            if f"W{i+1}" not in weights:
-                raise ValueError("Missing weights for layer", i+1)
+        if weights is None:
+            raise ValueError("Weights cannot be None")
 
-            layer.W = weights[f"W{i+1}"]
-            layer.b = weights[f"b{i+1}"]
+        for layer, w in zip(self.layers, weights):
+            layer.W = w["W"]
+            layer.b = w["b"]
     def forward(self, X):
         """
         Forward propagation through all layers.
